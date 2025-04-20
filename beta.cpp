78e822d2bcfa1d538572b4bc452af8e53c94a11d
Xandrea_san <ayezaavriellekim@gmail.com>
2025-04-20 16:38:43 +0800

    Update and rename beta to beta.cpp

--- beta.cpp
+++ beta.cpp
@@ -2,20 +2,39 @@ using System;
 
 public class HelloWorld
 {
+    
+        
     public static void Main(string[] args)
     {
+        Console.WriteLine ("ATM");
         Console.WriteLine ("Register");
         Console.Write("Name: ");
         string username = Console.ReadLine();
         Console.Write("Password: ");
         string password = Console.ReadLine();
         
-         Console.WriteLine ("Login");
+        
+        for(int i = 5; i > 0; i++){
+        Console.WriteLine ("Login");
         Console.Write("Name: ");
         string username1 = Console.ReadLine();
         Console.Write("Password: ");
         string password1 = Console.ReadLine();
         
+        if((username,password)==(username1,password1))
+        {
+            Console.WriteLine ("Hello " + username1 + " What do yuo like to do?");
         
-    }
-}
+            break;
+        }
+        else{
+           Console.WriteLine ("Wrong Usernmae or Password"); 
+        }
+        
+        
+        }
+        
+        
+        }
+            
+    } 
